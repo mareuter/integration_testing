c713f704e26e3902d42c766e04e906c98173eeb9
Michael Reuter <mareuter@lsst.org>
2020-08-18 16:05:16 -0700

    Linter improvements.

--- cc_nightstartup.py
+++ cc_nightstartup.py
@@ -3,27 +3,24 @@ import asyncio
 import logging
 import sys
 
-from astropy.time import Time
-
 from lsst.ts import salobj
 from lsst.ts.observatory.control.maintel.mtcs import MTCS, MTCSUsages
 from lsst.ts.observatory.control.maintel.comcam import ComCam, ComCamUsages
 
 async def main(opts):
-    
+
     stream_handler = logging.StreamHandler(sys.stdout)
     logger = logging.getLogger()
     logger.addHandler(stream_handler)
     logger.level = logging.DEBUG
-    
+
     domain = salobj.Domain()
-    
+
     start_tasks = []
-    
     if not opts.no_mt:
         mtcs = MTCS(domain, intended_usage=MTCSUsages().StartUp)
         start_tasks.append(mtcs.start_task)
-        
+
     if not opts.no_cc:
         comcam = ComCam(domain, intended_usage=ComCamUsages().StateTransition)
         start_tasks.append(comcam.start_task)
@@ -35,26 +32,26 @@ async def main(opts):
         mtcs.check.mtdometrajectory = False
 
     enable_tasks = []
-        
+
     if not opts.no_cc:
         ce = comcam.enable(settings={"cccamera": "Normal",
                                      "ccarchiver": "normal",
                                      "ccheaderservice": None})
         enable_tasks.append(ce)
-        
+
     if not opts.no_mt:
         me = mtcs.enable()
         enable_tasks.append(me)
 
     await asyncio.gather(*enable_tasks)
-    
+
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--no-cc", dest="no_cc", action="store_true")
     group.add_argument("--no-mt", dest="no_mt", action="store_true")
-    
+
     args = parser.parse_args()
-    
+
     asyncio.run(main(args))
