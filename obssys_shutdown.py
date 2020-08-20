f23becff2d3a4fbd55018a4168b8076ab2e4b29e
Michael Reuter <mareuter@lsst.org>
2020-08-19 17:23:49 -0700

    Add obssys startup/shutdown scripts.

---
+++ obssys_shutdown.py
@@ -0,0 +1,66 @@
+import argparse
+import asyncio
+import logging
+import sys
+
+from lsst.ts import salobj
+
+async def main(opts):
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger = logging.getLogger()
+    logger.addHandler(stream_handler)
+    logger.level = logging.DEBUG
+
+    domain = salobj.Domain()
+
+    start_tasks = []
+
+    domain = salobj.Domain()
+    if not opts.no_atq:
+        # AT ScriptQueue is index=2
+        atqueue = salobj.Remote(domain, 'ScriptQueue', index=2)
+        start_tasks.append(atqueue.start_task)
+
+    if not opts.no_mtq:
+        mtqueue = salobj.Remote(domain, 'ScriptQueue', index=1)
+        start_tasks.append(mtqueue.start_task)
+
+    if not opts.no_w:
+        watcher = salobj.Remote(domain, 'Watcher')
+        start_tasks.append(watcher.start_task)
+
+    await asyncio.gather(*start_tasks)
+
+    if opts.full:
+        shutdown_state = salobj.state.OFFLINE
+    else:
+        shutdown_state = salobj.state.STANDBY
+
+    shutdown_tasks = []
+    if not opts.no_atq:
+        task = salobj.set_summary_state(atqueue, shutdown_state)
+        shutdown_tasks.append(task)
+
+    if not opts.no_mtq:
+        task = salobj.set_summary_state(mtqueue, shutdown_state)
+        shutdown_tasks.append(task)
+
+    if not opts.no_w:
+        task = salobj.set_summary_state(watcher, shutdown_state)
+        shutdown_tasks.append(task)
+
+    await asyncio.gather(*shutdown_tasks)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--no-atq", dest="no_atq", action="store_true")
+    group.add_argument("--no-mtq", dest="no_mtq", action="store_true")
+    group.add_argument("--no-w", dest="no_w", action="store_true")
+    group.add_argument("--full", dest="full", action="store_true")
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args))
---
+++ obssys_startup.py
@@ -0,0 +1,62 @@
+import argparse
+import asyncio
+import logging
+import sys
+
+from lsst.ts import salobj
+
+async def main(opts):
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger = logging.getLogger()
+    logger.addHandler(stream_handler)
+    logger.level = logging.DEBUG
+
+    domain = salobj.Domain()
+
+    start_tasks = []
+
+    domain = salobj.Domain()
+    if not opts.no_atq:
+        # AT ScriptQueue is index=2
+        atqueue = salobj.Remote(domain, 'ScriptQueue', index=2)
+        start_tasks.append(atqueue.start_task)
+
+    if not opts.no_mtq:
+        mtqueue = salobj.Remote(domain, 'ScriptQueue', index=1)
+        start_tasks.append(mtqueue.start_task)
+
+    if not opts.no_w:
+        watcher = salobj.Remote(domain, 'Watcher')
+        start_tasks.append(watcher.start_task)
+
+    await asyncio.gather(*start_tasks)
+
+    enabled_state = salobj.state.ENABLED
+
+    enable_tasks = []
+    if not opts.no_atq:
+        task = salobj.set_summary_state(atqueue, enabled_state)
+        enable_tasks.append(task)
+
+    if not opts.no_mtq:
+        task = salobj.set_summary_state(mtqueue, enabled_state)
+        enable_tasks.append(task)
+
+    if not opts.no_w:
+        task = salobj.set_summary_state(watcher, enabled_state)
+        enable_tasks.append(task)
+
+    await asyncio.gather(*enable_tasks)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--no-atq", dest="no_atq", action="store_true")
+    group.add_argument("--no-mtq", dest="no_mtq", action="store_true")
+    group.add_argument("--no-w", dest="no_w", action="store_true")
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args))
